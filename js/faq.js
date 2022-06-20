57b71690d708891c4e0a4a16aa43a1c0ea16df52
deksn <eaakde@students.eaaa.dk>
2022-06-20 13:11:33 +0200

    Revert "Merge branch 'main' of https://github.com/razvanpolitic/schloss-oberndorf"
    This reverts commit 52b8048bc5bd0ec62fae966840bdc417ce37024b, reversing
    changes made to 0a701d93cbe1d49772d2f43cf7d406407add6664.

--- js/faq.js
+++ js/faq.js
@@ -10,14 +10,7 @@ for (i = 0; i < acc.length; i++) {
         if (panel.style.maxHeight) {
             panel.style.maxHeight = null;
         } else {
-            let active = document.querySelectorAll(".accordeon-animated-1 .accordeon-animated-button.active");
-            for(let j = 0; j < active.length; j++){
-                active[j].classList.remove("active");
-                active[j].nextElementSibling.style.maxHeight = null;
-            }
-            this.classList.toggle("active");
             panel.style.maxHeight = panel.scrollHeight + "px";
         }
     });
 }
-
